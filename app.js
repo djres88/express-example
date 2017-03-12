b25387813c9785f5d207363d0d4c8794205806e5
David Resnick <davidj.resnick@gmail.com>
2017-03-12 13:43:02 -0700

    pass template name as env to quickly test

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var index = require(`./${process.env.template}/routes/index`);
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, `${process.env.template}/views`));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -18,7 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, `${process.env.template}/public`)));
 
 app.use('/', index);
 
