0ebbec81fcc6397181943d84aa0c4c0420fb0616
djres88 <davidj.resnick@gmail.com>
2017-03-19 15:46:40 -0700

    update error/confirm message

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 const path = require('path')
+const exec = require('child_process').exec;
+
+//shared by each fn, just close over
 const destinationPath = path.dirname(require.main.filename);
 const templateName = argv[0];
 const assetPath = path.join(destinationPath, 'node_modules/start-bootstrap-node/assets', templateName);
-
-const exec = require('child_process').exec;
 
 function isValidTemplate() {
   exec(`ls ${assetPath}`, (error, stdout, stderr) => {
@@ -14,6 +15,12 @@ function isValidTemplate() {
     }      
   });
   return true;
+}
+
+function message() {
+    exec(`echo "copied files/folders for template ${templateName} to your project."`, (error, stdout, stderr) => {
+      console.log(stdout);
+    });
 }
 
 function copyFiles() {
@@ -27,5 +34,6 @@ function copyFiles() {
         console.error(`error while getting pwd: ${error}`);
         return;
       }
+      message();
   })
 }
